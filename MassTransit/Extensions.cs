30bd28e1bdd7b46872433bef08e9fb99f62e3094
Whaylon Coleman <colemanwhaylon@yahoo.com>
2023-01-27 15:12:31 -0800

    Add Azure Service Bus support.

--- MassTransit/Extensions.cs
+++ MassTransit/Extensions.cs
@@ -13,6 +13,30 @@ namespace Play.Common.MassTransit
 {
     public static class Extensions
     {
+        private const string RabbitMq = "RABBITMQ";
+        private const string ServiceBus = "SERVICEBUS";
+
+        public static IServiceCollection AddMassTransitWithMessageBroker(
+            this IServiceCollection services,
+            IConfiguration config,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            var serviceSettings = config.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            switch (serviceSettings.MessageBroker?.ToUpper())
+            {
+                case ServiceBus:
+                    services.AddMassTransitWithServiceBus(configureRetries);
+                    break;
+                case RabbitMq:
+                default:
+                    services.AddMassTransitWithRabbitMq(configureRetries);
+                    break;                
+            }
+
+            return services;
+        }
+
         public static IServiceCollection AddMassTransitWithRabbitMq(
             this IServiceCollection services,
             Action<IRetryConfigurator> configureRetries = null)
@@ -26,6 +50,40 @@ namespace Play.Common.MassTransit
             services.AddMassTransitHostedService();
 
             return services;
+        }
+
+        public static IServiceCollection AddMassTransitWithServiceBus(
+            this IServiceCollection services,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+                configure.UsingPlayEconomyAzureServiceBus(configureRetries);
+            });
+
+            services.AddMassTransitHostedService();
+
+            return services;
+        }        
+
+        public static void UsingPlayEconomyMessageBroker(
+            this IServiceCollectionBusConfigurator configure,
+            IConfiguration config,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            var serviceSettings = config.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            switch (serviceSettings.MessageBroker?.ToUpper())
+            {
+                case ServiceBus:
+                    configure.UsingPlayEconomyAzureServiceBus(configureRetries);
+                    break;
+                case RabbitMq:
+                default:
+                    configure.UsingPlayEconomyRabbitMq(configureRetries);
+                    break;                
+            }
         }
 
         public static void UsingPlayEconomyRabbitMq(
@@ -48,5 +106,26 @@ namespace Play.Common.MassTransit
                 configurator.UseMessageRetry(configureRetries);
             });
         }
+
+        public static void UsingPlayEconomyAzureServiceBus(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            configure.UsingAzureServiceBus((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var serviceBusSettings = configuration.GetSection(nameof(ServiceBusSettings)).Get<ServiceBusSettings>();
+                configurator.Host(serviceBusSettings.ConnectionString);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+
+                if (configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
+        }        
     }
 }
--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -2,23 +2,17 @@ namespace Play.Common.Settings
 {
     public class MongoDbSettings
     {
+        private string connectionString;
+
         public string Host { get; init; }
 
         public int Port { get; init; }
 
-        // public string ConnectionString => $"mongodb://{Host}:{Port}";
-        private string connectionString;
         public string ConnectionString
         {
-            get
-            {
-                return string.IsNullOrWhiteSpace(connectionString) ?
-                    $"mongodb://{Host}:{Port}" : connectionString;
-            }
-            init
-            {
-                connectionString = value;
-            }
+            get { return string.IsNullOrWhiteSpace(connectionString) 
+                    ? $"mongodb://{Host}:{Port}" : connectionString; }
+            init { connectionString = value; }
         }
 
     }
